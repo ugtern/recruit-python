ae0d92760a24ee0e2564a0780961e63d352f63c5
hids <Ugtern@gmail.com>
2019-04-17 10:54:29 +0400

    added new request post's form

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-import time
+import time,json
+from urllib.request import urlopen
 
 class App:
     def show(self, environ, start_response):
@@ -18,6 +19,11 @@ class App:
         length = int(environ['CONTENT_LENGTH'])
         body = environ['wsgi.input'].read(length).decode('utf-8')
 
+        print(environ['wsgi.input'],type(environ['wsgi.input']),length)
+
+        print(urlopen(environ['wsgi.input']).read(length).decode('utf-8'))
+
+        """
         mass = [i.split('=') for i in body.split('&')]
         data_time = time.strftime("%Y-%m-%d %H.%M", time.localtime(int(mass[0][1]))).split(' ')
 
@@ -29,6 +35,7 @@ class App:
         f.write('Отправитель: '+mass[1][1].replace('.',' ')+' Дата: '+data_time[0]+' Время: '+data_time[1]+'\n')
         f.close()
 
+        """
         # Обработка сохранения здесь. В body находится тело запроса в виде строки.
 
         start_response(status, [('Content-Length', '0')])
--- request.py
+++ request.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import json
 
 test_data = [
   {
@@ -19,12 +20,40 @@ test_data = [
   }
 ]
 
-url = 'http://localhost:8080/' # URL
-for i in test_data:
-    print(i)
-    post_fields = i   # POST
+#print(dict([('1',i)] for i in test_data))
 
-    request = Request(url, urlencode(post_fields).encode())
+d={i:json.dumps(test_data[i], separators=(',',':')) for i in range(len(test_data))}
+print(d,type(d))
+
+"""
+j=0
+for i in test_data:
+    req = dict([(j,json.dumps(i, separators=(',',':')))])
+    j+=1
+
+print(req,type(req))
+"""
+
+url = 'http://localhost:8080/' # URL
+
+def test_1(test_data):
+    for i in test_data:
+        print(i)
+        post_fields = i   # POST
+
+        request = Request(url, urlencode(post_fields).encode())
+        json = urlopen(request).read().decode()
+
+        print(json)
+
+def test_2(test_data):
+    post_fields = test_data  # POST
+
+    request = Request(url, urlencode(post_fields).encode('utf-8'))
+    print(request,type(request))
     json = urlopen(request).read().decode()
 
     print(json)
+
+#test_2(d)
+test_1(test_data)
